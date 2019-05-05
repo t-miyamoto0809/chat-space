1c6ddd2af41cc99bc9a559072800c79645b4f68f
t-miyamoto0809 <t.miyamoto0809@gmail.com>
2019-05-05 14:10:10 +0900

    イベント時に非同期通信

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,11 @@
 $(function(){
   $(".chat-group-form__input").on("keyup", function(e){
     var input = $(".chat-group-form__input").val(); //val()でフォームの値を取得
-    console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users/index',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
