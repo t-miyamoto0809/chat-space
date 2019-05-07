770041db9e71548bbeb2259997b7f7e0af19e198
t-miyamoto0809 <t.miyamoto0809@gmail.com>
2019-05-07 21:05:36 +0900

    自動更新の実装

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,11 +1,15 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_group
+
   def index
-    @group = Group.find(params[:group_id])
-    last_message_id = params[:id]
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages
     respond_to do |format|
       format.html
-      format.json{ @messages = @messages.where('id > ?', params[:id]) }
+      format.json { @messages = @messages.where('id > ?', params[:id])}
     end
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
